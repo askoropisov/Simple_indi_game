b95e7242340834d6851ac144651b4ee289ae20a3
Артем Cкорописов <sckoropisow.ar@yandex.ru>
2021-07-29 18:37:02 +0300

    добавил спавн артефактов как объектов

--- Simple_indi_game/Main.cpp
+++ Simple_indi_game/Main.cpp
@@ -18,17 +18,27 @@ public:
 
 class Player {
 public:
-	int x_pos;
-	int y_pos; 
+	int x_pos = 0;
+	int y_pos = 0; 
 
-};
+} player;
+
+
 
 class Artifact {
 public:
-	int x_pos;
-	int y_pos;
+	int number = 0;
+	int x_pos = 0;
+	int y_pos = 0;
+	bool check_for_take = false;
+	void create(vector<vector<char>>& DRP);
+public:
+	Artifact(int number);
 	
 };
+Artifact::Artifact(int number) : number(number) {}
+
+vector<Artifact*> artifacts;
 
 vector<vector<char>> Map::Read_file(ifstream& in_file) {                  //reading input file and create gameng map
 	char symbol;
@@ -51,6 +61,24 @@ vector<vector<char>> Map::Read_file(ifstream& in_file) {                  //read
 	return DRP;
 }
 
+void Artifact::create(vector<vector<char>>& DRP) {
+	
+	int temp_x, temp_y, counter = 0;
+	srand(time(NULL));
+
+	while (counter < 1) {
+		temp_x = rand() % map.height;
+		temp_y = rand() % map.length;
+		if (DRP[temp_x][temp_y] == '.') {
+			x_pos = temp_x;
+			y_pos = temp_y;
+			DRP[x_pos][y_pos] = '+';
+			counter++;
+		}
+	}
+
+} 
+
 void Map::Out_map(vector<vector<char>> DRP) {							
 	for (int j = 0; j < map.height; j++) {
 		for (int i = 0; i < map.length; i++) {
@@ -58,17 +86,25 @@ void Map::Out_map(vector<vector<char>> DRP) {
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
 int main() {
 	vector<vector<char>> DRP_symbol = map.Read_file(in_file);
 	vector<vector<int>> DRP;
 
-	DRP_symbol[0][0] = 'P';												
+	DRP_symbol[player.x_pos][player.y_pos] = 'P';												
 	map.Out_map(DRP_symbol);
 
+	for (int i = 0; i < 3; i++) {
+		Artifact* art = new Artifact(i);
+		artifacts.push_back(art);
 
-
+		art->create(DRP_symbol);
+	}
+	
+	map.Out_map(DRP_symbol);
+	
 
 	
 }
