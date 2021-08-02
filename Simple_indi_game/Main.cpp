8752d045a118248f6cba117dda6f419a5f9376de
Артем Cкорописов <sckoropisow.ar@yandex.ru>
2021-08-02 19:52:04 +0300

    Разобрался с This, уменьшил мейн, и кол-во глобальных переменных, добавил перечисления.

--- Simple_indi_game/Main.cpp
+++ Simple_indi_game/Main.cpp
@@ -9,33 +9,13 @@
 
 using namespace std;
 
-
-
-class Map {
-public:
-	HANDLE h_console;
-	int length;
-	int height;
-	int exit_x_pos = -10;
-	int exit_y_pos = -10;
-	char barier_target = '#';
-	char void_target = '.';
-	vector<vector<char>> DRP;
-
-	vector<vector<char>> Read_file(ifstream& in_file);                       //reading file and return DRP[][]
-	void out_map(vector<vector<char>> DRP);
-	void create_exit(vector<vector<char>>& DRP);
-} map;
-
 class Player {
 public:
 	int x_pos = 0;
 	int y_pos = 0;
-	char target = 'P';
 
 	void movement(vector<vector<char>>& DRP, int key, int trash);
-} player;
-
+};
 
 class Artifact {
 public:
@@ -44,120 +24,152 @@ public:
 	int y_pos = 0;
 
 	void create(vector<vector<char>>& DRP);
-	Artifact(int number);	
+	Artifact(int number);
 	~Artifact() {};
 };
 Artifact::Artifact(int number) : number(number) {}
 
+class Map {
+public:
+	enum symbol {
+		barier='#',
+		void_cell='.',
+		player='P',
+		artifact='@',
+		exit='E',
+	};
+	enum color {
+		black,
+		blue,
+		green,
+		white_blue,
+		red,
+		purple,
+		yelow,
+		white,
+		gray,
+		light_blue,
+		light_green,
+		light_white_blue,
+		light_red,
+		pink,
+		light_yelow,
+		light_white
+	};
+	HANDLE h_console;
+	int length;
+	int height;
+	int exit_x_pos = -10;
+	int exit_y_pos = -10;
+	vector<vector<char>> DRP;
+
+	bool create(ifstream& in_file, Player player, vector<Artifact*>& artifacts);
+	void read_file(ifstream& in_file);                       //reading file and return DRP[][]
+	void print(vector<vector<char>> DRP);
+	void create_exit(vector<vector<char>>& DRP);
+	void color_symbol(vector<vector<char>>& DRP, int color, int i, int j);
+} map;
+
 
 void Player::movement(vector<vector<char>>& DRP, int key, int trash) {
-	int temp_x = player.x_pos;
-	int temp_y = player.y_pos;
+	int temp_x = this->x_pos;
+	int temp_y = this->y_pos;
 	switch (key) {
 	case 72:
-		if (player.x_pos > 0 && DRP[player.x_pos-1][player.y_pos] != map.barier_target) {
-			player.x_pos -= 1;
-			//Beep(5000, 50);
+		if (this->x_pos > 0 && DRP[this->x_pos-1][this->y_pos] != map.barier) {
+			this->x_pos -= 1;
 		}
 		break;
 	case 80:
-		if (player.x_pos < map.height - 1 && DRP[player.x_pos + 1][player.y_pos] != map.barier_target) {
-			player.x_pos += 1;
-			//Beep(5000, 50);
+		if (this->x_pos < map.height - 1 && DRP[this->x_pos + 1][this->y_pos] != map.barier) {
+			this->x_pos += 1;
 		}
 		break;
 	case 75:
-		if (player.y_pos > 0 && DRP[player.x_pos][player.y_pos-1] != map.barier_target) {
-			player.y_pos -= 1;
-			//Beep(5000, 50);
+		if (this->y_pos > 0 && DRP[this->x_pos][this->y_pos-1] != map.barier) {
+			this->y_pos -= 1;
 		}
 		break;
 	case 77:
-		if (player.y_pos < map.length - 1 && DRP[player.x_pos][player.y_pos + 1] != map.barier_target) {
-			player.y_pos += 1;
-			//Beep(5000, 50);
+		if (this->y_pos < map.length - 1 && DRP[this->x_pos][this->y_pos + 1] != map.barier) {
+			this->y_pos += 1;
 		}
 		break;
 	default:
 		break;
 	}
-	DRP[temp_x][temp_y] = map.void_target;
-	DRP[player.x_pos][player.y_pos] = player.target;
+	DRP[temp_x][temp_y] = map.void_cell;
+	DRP[this->x_pos][this->y_pos] = map.player;
 }
 
-void Artifact::create(vector<vector<char>>& DRP) {	
+void Artifact::create(vector<vector<char>>& DRP) {
 	int temp_x, temp_y, counter = 0;
 	srand(time(NULL));
 
 	while (counter < 1) {
 		temp_x = rand() % map.height;
 		temp_y = rand() % map.length;
-		if (DRP[temp_x][temp_y] == map.void_target && DRP[temp_x][temp_y] != DRP[0][0]) {
+		if (DRP[temp_x][temp_y] == map.void_cell && DRP[temp_x][temp_y] != DRP[0][0]) {
 			x_pos = temp_x;
 			y_pos = temp_y;
-			DRP[x_pos][y_pos] = '@';
+			DRP[x_pos][y_pos] = map.artifact;
 			counter++;
 		}
 	}
 } 
 
-vector<vector<char>> Map::Read_file(ifstream& in_file) {                      //reading input file and create gaming map
-
+void Map::read_file(ifstream& in_file) {                      //reading input file and create gaming map
+	
 	char symbol;
-	vector<vector<char>> DRP;
-	in_file >> map.length;
-	in_file >> map.height;
+	in_file >> this->length;
+	in_file >> this->height;
 	vector<char> string;
 
 	symbol = in_file.get();                                               //skip '\n'
-	for (int j = 0; j < map.height; j++) {
-		for (int i = 0; i < map.length; i++) {
+	for (int j = 0; j < this->height; j++) {
+		for (int i = 0; i < this->length; i++) {
 			symbol = in_file.get();
 			string.push_back(symbol);
 		}
 		symbol = in_file.get();                                           //skip '\n' in end string
-		DRP.push_back(string);
+		this->DRP.push_back(string);
 		string.clear();
 	}
 	in_file.close();
-	return DRP;
 }
 
-void Map::out_map(vector<vector<char>> DRP) {
+void Map::color_symbol(vector<vector<char>>& DRP, int color, int i, int j) {
 	h_console = GetStdHandle(STD_OUTPUT_HANDLE);
-	for (int j = 0; j < map.height; j++) {
+	SetConsoleTextAttribute(h_console, color);
+	cout << setw(2) << DRP[j][i];
+	SetConsoleTextAttribute(h_console, color::white);
+}
+
+void Map::print(vector<vector<char>> DRP) {
+
+	for (int j = 0; j < this->height; j++) {
 		cout <<"                                      ";
-		for (int i = 0; i < map.length; i++) {
+		for (int i = 0; i < this->length; i++) {
 			switch (DRP[j][i])
 			{
-			case '@': {
-				SetConsoleTextAttribute(h_console, 4);
-				cout << setw(2) << DRP[j][i];
-				SetConsoleTextAttribute(h_console, 7);
+			case symbol::artifact: {
+				this->color_symbol(DRP, color::red, i, j);
 				break;
 			}
-			case 'P': {
-				SetConsoleTextAttribute(h_console, 2);
-				cout << setw(2) << DRP[j][i];
-				SetConsoleTextAttribute(h_console, 7);
+			case symbol::player: {
+				this->color_symbol(DRP, color::green, i, j);
 				break;
 			}
-			case 'E': {
-				SetConsoleTextAttribute(h_console, 5);
-				cout << setw(2) << DRP[j][i];
-				SetConsoleTextAttribute(h_console, 7);
+			case symbol::exit: {
+				this->color_symbol(DRP, color::purple, i, j);
 				break;
 			}
-			case '#': {
-				SetConsoleTextAttribute(h_console, 6);
-				cout << setw(2) << DRP[j][i];
-				SetConsoleTextAttribute(h_console, 7);
+			case symbol::barier: {
+				this->color_symbol(DRP, color::yelow, i, j);
 				break;
 			}
-			case '.': {
-				SetConsoleTextAttribute(h_console, 9);
-				cout << setw(2) << DRP[j][i];
-				SetConsoleTextAttribute(h_console, 7);
+			case symbol::void_cell: {
+				this->color_symbol(DRP, color::light_blue, i, j);
 				break;
 			}
 			default:
@@ -175,18 +187,46 @@ void Map::create_exit(vector<vector<char>>& DRP) {
 	srand(time(NULL));
 
 	while (counter < 1) {
-		temp_x = rand() % map.height;
-		temp_y = rand() % map.length;
-		if (DRP[temp_x][temp_y] == map.barier_target) {
-			map.exit_x_pos = temp_x;
-			map.exit_y_pos = temp_y;
-			DRP[exit_x_pos][exit_y_pos] = 'E';
+		temp_x = rand() % this->height;
+		temp_y = rand() % this->length;
+		if (DRP[temp_x][temp_y] == this->barier) {
+			this->exit_x_pos = temp_x;
+			this->exit_y_pos = temp_y;
+			DRP[exit_x_pos][exit_y_pos] = symbol::exit;
 			counter++;
 		}
 	}
 }
 
-void simple_music() {
+bool Map::create(ifstream& in_file, Player player, vector<Artifact*>& artifacts) {
+
+	int counter_artifacts = 0;
+
+	if (!in_file.is_open()) {
+		cout << "The file cannot be opened" << endl;
+		return EXIT_FAILURE;
+	}
+	else if (in_file.peek() == EOF) {
+		cout << "File empty" << endl;
+		return EXIT_FAILURE;
+	}
+	else {
+		map.read_file(in_file);
+	}
+
+	this->DRP[player.x_pos][player.y_pos] = this->player;                                      //set Player
+	this->print(this->DRP);
+
+	for (int i = 0; i < 3; i++) {                                                             //create 3 object artifact
+		Artifact* art = new Artifact(i);
+		artifacts.push_back(art);
+
+		art->create(this->DRP);
+	}
+	this->print(this->DRP);
+}
+
+void simple_music_back() {
 	while (true) {
 		Beep(784, 150);
 		Sleep(300);
@@ -236,77 +276,63 @@ void simple_music() {
 		Beep(523, 150);
 	}
 }
+void simple_music_win() {
+	Beep(659.26, 200);
+	Beep(659.26, 200);
+	Sleep(200);
+	Beep(659.26, 200);
+	Sleep(100);
+	Beep(523.26, 200);
+	Beep(659.26, 200);
+	Sleep(200);
+	Beep(783.98, 200);
+	Sleep(400);
+	Beep(391.99, 200);
+}
+
+void victory() {
+	simple_music_win();
+
+	SetConsoleTextAttribute(map.h_console, map.purple);
+	cout << endl << endl << endl << endl << endl << endl << setw(62) << "YOU WIN!"
+		<< endl << endl << endl << endl << endl << endl << endl << endl << endl;
+	SetConsoleTextAttribute(map.h_console, map.white);
+}
 
 
 int main() {
 
-	thread music(simple_music);
+	//Map map;
+	Player player;
+	thread music(simple_music_back);
+
 	ifstream in_file("input_medium.txt");
 	vector<Artifact*> artifacts;
+
+	map.create(in_file, player, artifacts);
 	int counter_artifacts = 0;
 
-	if (!in_file.is_open()) {
-		cout << "The file cannot be opened" << endl;
-		return EXIT_FAILURE;
-	}
-	else if (in_file.peek() == EOF) {
-		cout << "File empty" << endl;
-		return EXIT_FAILURE;
-	}
-	else {
-			map.DRP = map.Read_file(in_file);
-	}
-
-	map.DRP[player.x_pos][player.y_pos] = player.target;                                      //set Player 												
-	map.out_map(map.DRP);
-
-	for (int i = 0; i < 3; i++) {                                                             //create 3 object artifact
-		Artifact* art = new Artifact(i);
-		artifacts.push_back(art);
-
-		art->create(map.DRP);
-	}
-	map.out_map(map.DRP);
 
 	while (true) {
-
 		player.movement(map.DRP, _getch(), _getch());                                          //second _getch for for the special feature of arrow processing
 		for (int i = 0; i < artifacts.size(); i++) {
 			if (player.x_pos == artifacts[i]->x_pos && player.y_pos == artifacts[i]->y_pos) {
-				//Beep(250, 20);
 				counter_artifacts++;
 				delete artifacts[i];
 			}
 		}
 
-		map.out_map(map.DRP);
+		map.print(map.DRP);
 
 		if (counter_artifacts == 3) {
 			map.create_exit(map.DRP);
-			map.out_map(map.DRP);
+			map.print(map.DRP);
 			counter_artifacts = 0;
 		}
 
 		if (player.x_pos == map.exit_x_pos && player.y_pos == map.exit_y_pos) {
 			music.detach();
-
-			Beep(659.26, 200);
-			Beep(659.26, 200);
-			Sleep(200);
-			Beep(659.26, 200);
-			Sleep(100);
-			Beep(523.26, 200);
-			Beep(659.26, 200);
-			Sleep(200);
-			Beep(783.98, 200);
-			Sleep(400);
-			Beep(391.99, 200);
-
-			SetConsoleTextAttribute(map.h_console, 5);
-			cout << endl << endl << endl << endl << endl << endl << setw(62) << "YOU WIN!"
-				<< endl << endl << endl << endl << endl << endl << endl << endl << endl;
-			SetConsoleTextAttribute(map.h_console, 7);
-
+			victory();
 			break;
 		}
 	}
